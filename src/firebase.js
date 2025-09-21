ed05b265375b80b06181492add6b9261e3d7759f
tintayadev <pau.tintaya.code@gmail.com>
2025-09-21 06:26:55 -0400

    chore(firebase): add hosting config and Firebase bootstrap

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB8J7CNMayLHPKZsdUZ6oyqRASjy5ekjgw",
+  authDomain: "taqi-travels.firebaseapp.com",
+  projectId: "taqi-travels",
+  storageBucket: "taqi-travels.firebasestorage.app",
+  messagingSenderId: "678805942113",
+  appId: "1:678805942113:web:be538380ac4230b90e1f20"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
